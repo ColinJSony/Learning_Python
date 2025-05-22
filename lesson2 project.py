d226bc5980f039757443915f1e2595df56e8c14d
ColinJSony <157513201+ColinJSony@users.noreply.github.com>
2025-05-21 20:20:10 -0500

    Update lesson2 project.py

--- lesson2 project.py
+++ lesson2 project.py
@@ -1,4 +1,13 @@
- math_statement = input("Enter in a math statement: ")
+def add(a+b)
+ return a + b
+def sub(a-b)
+ return a - b
+def div(a/b)
+ return a / b
+def mul(a*b)
+ return a * b
+
+math_statement = input("Enter in a math statement: ")
 
 math_terms = math_statement.split(" ")
 print(math_terms)
@@ -8,13 +17,13 @@ y = float(math_terms[2])
 
 
 if op == "*":
-    print(x * y)
+    print(mul(x,y))
 elif op == "/": 
-    print(x / y)
+    print(div(x,y))
 elif op == "+":
-    print(x + y)
+    print(add(x,y))
 elif op == ("-"): 
-    print(x - y)
+    print(sub(x,y))
 else: 
     print('invalid operation')
 
